ac8d84698dc41e0cfcc64be5aed50a61f3f2d77b
manupadillaph <manuepad@gmail.com>
2025-04-21 14:42:44 -0300

    chore: initialize project with Next.js and TypeScript

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
+const webpack = require("webpack");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    //-------------
+    // PRODUCTION
+    // swcMinify: true,
+    // reactStrictMode: true,
+    //-------------
+    // DEV
+    reactStrictMode: false,
+    //-------------
+    /* config options here */
+    webpack: (config, { dev, isServer }) => {
+        config.ignoreWarnings = [
+            {
+                module: /typeorm/,
+                message: /Module not found|dependency is an expression/,
+            },
+        ];
+
+        if (dev) {
+            config.devtool = "eval-source-map"; // Fast for development
+        } else {
+            config.devtool = "source-map"; // Detailed for production
+        }
+
+        return config;
+    },
 };
 
 export default nextConfig;
