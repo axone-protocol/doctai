0ecc98a2627194a71dc16f252f13244067d07ac7
manupadillaph <manuepad@gmail.com>
2025-04-21 14:45:27 -0300

    feat(auth): create login page and user session management

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,52 @@
+// hooks/useAuth.ts
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter, usePathname } from "next/navigation";
+import { useUserStore } from "@/store/useUserStore";
+import { fetchEntities } from "@/lib/frontend/api";
+import { User } from "@/entities/User";
+
+export function useAuth({ autoRedirect = true } = {}) {
+    const { data: session, status } = useSession();
+    const { address, userType, setAddress, setUserType,setUser, reset } =
+        useUserStore();
+    const router = useRouter();
+    const pathname = usePathname();
+    const [initialized, setInitialized] = useState(false);
+
+    useEffect(() => {
+        if (status === "loading") return;
+        const hydrateUser = async () => {
+            if (session?.user?.address) {
+                // Use generic entity filter to fetch user from DB
+                const users = await fetchEntities<User>(User.name, {
+                    filter: { address: session.user.address },
+                  });
+                const dbUser = users[0];
+
+                if (dbUser) {
+                    setAddress(session.user.address);
+                    setUserType(session.user.userType);
+                    setUser(dbUser);
+                }
+            } else if (autoRedirect && pathname !== "/login") {
+                router.replace("/login");
+            }
+
+            setInitialized(true);
+        };
+
+        hydrateUser();
+        setInitialized(true);
+    }, [status, session, pathname]);
+
+    return {
+        address,
+        userType,
+        isAuthenticated: !!address,
+        initialized,
+        setAddress,
+        setUserType,
+        reset,
+    };
+}
