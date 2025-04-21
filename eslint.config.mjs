d185a57b5b7a37e5a23e88aa2c2661a2913c6142
manupadillaph <manuepad@gmail.com>
2025-04-21 14:42:57 -0300

    build: add linting, ESLint config, commitlint, and GitHub Actions workflows

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends(
+        "next/core-web-vitals",
+        "plugin:@next/next/recommended"
+        // "next/typescript"
+    ),
+    {
+        // This overrides rules globally
+        rules: {
+            "react-hooks/exhaustive-deps": "off",
+        },
+    },
 ];
 
 export default eslintConfig;
